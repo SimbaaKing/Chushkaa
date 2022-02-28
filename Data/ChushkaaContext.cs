f12928347391b2c592673f7b25d5d62a9f883e23
SimbaaKing <bozikovagabriela400@gmail.com>
2022-02-28 18:34:03 +0200

    comm1

---
+++ Data/ChushkaaContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chushkaa.Data
+{
+    public class ChushkaaContext:DbContext
+    {
+        public ChushkaaContext(DbContextOptions<ChushkaaContext>op) : base(op)
+        {
+
+        }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+    }
+
+}
